3296852f75bda9d74b0cf1c4e8480ee3160f80c5
ns-masthan <141628672+ns-masthan@users.noreply.github.com>
2023-09-28 11:41:37 +0530

    Add files via upload

---
+++ Day1_Batch82.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello all wellcome to python programinig language")
+
+
+# In[3]:
+
+
+frist name = "masthan"
+print(frist name)
+
+
+# In[4]:
+
+
+fristname = 'masthan'
+print(fristname)
+
+
+# In[5]:
+
+
+@name = "masthan"
+print(@name)
+
+
+# In[6]:
+
+
+name@ = "masthan"
+print(name@)
+
+
+# In[7]:
+
+
+name25 ="masthan"
+print(name25)
+
+
+# In[8]:
+
+
+25name = "masthan"
+print(25name)
+
+
+# In[9]:
+
+
+a='masthan'
+print(a)
+
+
+# In[10]:
+
+
+type(a)
+
+
+# In[11]:
+
+
+s="mass"
+print(s)
+
+
+# In[12]:
+
+
+type(s)
+
+
+# In[ ]:
+
+
+
+
